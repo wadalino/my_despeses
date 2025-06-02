d3f033408a707a849578fdef19e1a1733e39e2ee
aproscom <aproscom@wadalino.org>
2025-06-02 16:22:33 +0200

    1.5 beta 1 some changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+server: {
+     host: '0.0.0.0', // Escucha en todas las interfaces de red
+     port: 5173, // Puerto (puedes cambiarlo si es necesario)
+    open: true, // Abre automáticamente en el navegador en la máquina local
+     proxy: {
+       '/api': {
+         target: 'http://192.168.1.26:5173',
+         changeOrigin: true,
+         rewrite: (path) => path.replace(/^\/api/, '')
+       }
+     },
+ 
+   },
   plugins: [react()],
 })
+
+
